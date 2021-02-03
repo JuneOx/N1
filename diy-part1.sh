0c5c952834bfd49c2e0e3977a3ab35f3af822287
JuneOx <chengliye@gmail.com>
2021-02-03 21:48:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/tty228/luci-app-serverchan.git packages/lean/
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/
 git clone https://github.com/tuanqing/install-program package/install-program
-git clone https://github.com/tty228/luci-app-serverchan.git
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
+sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
